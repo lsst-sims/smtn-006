c94f3446e99bcd9eb2fc1f98eeb1913f00f258bd
danielsf <scott.f.daniel@gmail.com>
2016-08-05 15:14:12 -0700

    added docstring to generate_ebv_max.py

--- software/fitting/generate_ebv_max.py
+++ software/fitting/generate_ebv_max.py
@@ -1,3 +1,18 @@
+"""
+This script is called by fit_bright_stars.cpp to calculate the integrated
+E(B-V) dust extinction along a line of sight.  fit_bright_stars.cpp then uses
+that value as a cutoff when searching the E(B-V) grid for best fit values for
+particular stars.
+
+The call signature of the script is
+
+python generate_ebv_max.py input_file output_file
+
+where input_file is one of the original .csv.gz files as provided by Dave
+Monet and output_file is a text file where, for each star in input_file, the
+maximum E(B-V) value will be listed.
+"""
+
 import sys
 import gzip
 import numpy as np
